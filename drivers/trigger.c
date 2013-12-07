de5b274ebd777acf0141f2240321d306a0198a4b
Lars Bratrud <uberfryx@gmail.com>
2013-12-07 23:08:47 +0100

    added some stuff to rcuc_read(), dont know what i am doing

--- drivers/trigger.c
+++ drivers/trigger.c
@@ -8,75 +8,75 @@
 #include <linux/cdev.h>      // 
 #include <asm/uaccess.h>     // user space memory access
 #include <asm/io.h>          // ioremap etc.
+#include <linux/moduleparam.h>
+
 
 #include "mrKernLogging.c"   // kernel debugging
 
 /* Physical addresses */
-static u32* rcu_trig_recv_physaddr = ((u32 *) 0x50040000);
-static int  rcu_trig_recv_size     = 0x10; // size of 16 for testing 
+static u32* trig_recv_phys_base = ((u32 *) 0x50040000);
+static int  trig_recv_size     = 0x10; // size of 16 for testing 
 /* Virtual addresses */
-static u32* rcu_trig_recv_virtbase = NULL;
+static u32* trig_recv_virtbase = NULL;
 
 // Fixed major number
 static uint rcuc_majorID = 254;
 
+/* Parameters which can be changed at driver load */
+//smodule_param(trig_recv_phys_base, long, 0);
+
+
 // Access control
 static int driver_lock = 0;
-
-static void __iomem *rcu;
-static dev_t first; // Global variable for first minor device number
-static struct cdev c_device; // Global variable for the char device structure
-static struct class *c1; // Global variable for the device class
-
-
-/*Internal functions*/
-/* read from memory
- * the readl and readb functions are provided by the system and must be used to read from 
- * io memory initialized by ioremap (see init_module)
- * parameters:
- *  begin: start address
- *  size:  size of the buffer in BYTE
- *  buff:  pointer to buffer that receives data
- */
-static int dcs_read (u32 begin, u32 size, u32* buff) 
-{
-  int i;
-  u32 wordoffs=0;
-  for (i=0;i<size/4;i++){ // do the multiples of 4 
-    *(buff+wordoffs) = readl(begin+i*4);
-    wordoffs++;    
-  }
-  i*=4;
-  u32 byteoffs=0; 
-  for (;i<size;i++) { // do the remaining bytes
-    *(((char*)(buff+wordoffs))+byteoffs)=readb(begin+i);
-    byteoffs++;
-  }
-  return 0;
-}
-
-
 
 /*
 * Here comes file operation functions:
 */
 // File open
-static int rcuc_open(struct inode *inode, struct file *file)
+static int rcuc_open(struct inode* inode, struct file* file_p)
 {
 	return 0;
 }
 // File close 
-static int rcuc_close(struct inode *inode, struct file *file)
+static int rcuc_close(struct inode* inode, struct file* file_p)
 {
 	return 0;
 }
 // File read
-static ssize_t rcuc_read(struct file *file, 
-                         char __user *buffer, 
-                         size_t length, 
+static ssize_t rcuc_read(struct file* file_p, 
+                         char __user* buffer, 
+                         size_t count, 
                          loff_t *offset)
 {
-	return 0;
+	// some stuff
+	int iResult = 0;
+	u32 lPosition=file_p->f_pos;
+	u32 u32Offset=0;
+	u32 u32Count=0;
+	u32* pu32Source=NULL;
+	
+	if(file_p==NULL){ // check the file pointer
+		printk("read: invalid file pointer\n");
+		return -EFAULT;
+	}
+	if(file_p && buffer && count>=0 && lPosition>=0){
+		printk("read: count=%d f_pos=0x%08x", count, lPosition);
+		if(lPosition < trig_recv_size){
+			if(count + lPosition <= trig_recv_size)
+			  u32Count = count;
+			else{
+			  u32Count = trig_recv_size;
+			  printk("read: read access exceeds the size of the register, truncate");
+			}
+		u32Offset = lPosition;
+		pu32Source = trig_recv_phys_base;
+		}
+	}
+	else{
+		printk("read: invalid parameter\n");
+		iResult=-EINVAL;
+	}
+	return iResult;
 }
 // File write
 static ssize_t rcuc_write(struct file *file, 
@@ -102,8 +102,8 @@ static struct file_operations rcuc_fops =
 static int __init rcumodule_init(void)
 {
 	int iResult = 0; // holding result of operations
-	u32* buffer;
-	u32 u32Count = 1;
+	u32 buffer;
+	//u32 u32Count = 1;
 	
 	
 	// Register the driver
@@ -115,30 +115,31 @@ static int __init rcumodule_init(void)
 	else{
 		printk(KERN_INFO "module init: success!\n");
 		//mrlogmessage(LOG_INFO, KERN_INFO, "module init: success!");
+		
+		// Map physical address to virtual address, not needed, InitRealBuffers()
+		if(trig_recv_size){
+			trig_recv_virtbase = (u32*) ioremap_nocache( (u32 *)trig_recv_phys_base, 				(int)trig_recv_size );
+		printk("Remapped TRGRECV from 0x%p to 0x%p\n", trig_recv_phys_base, trig_recv_virtbase);
+		}
+		// InitDriverLock()
+
+	
+
+	buffer = readl(trig_recv_virtbase);
+	printk("read %lx, at 0x%p\n", (long unsigned int)buffer, trig_recv_virtbase);
+//	iResult = initRealBuffers();
+		
 	}
 	
-	// Map physical address to virtual address
-	if(rcu_trig_recv_size){
-		rcu_trig_recv_virtbase = (u32*) ioremap_nocache( (u32 *)rcu_trig_recv_physaddr, rcu_trig_recv_size );
-		printk("Remapped TRGRECV from 0x%p to 0x%p\n", rcu_trig_recv_physaddr, rcu_trig_recv_virtbase);
-	}
-	// try to read some stuff
-	//dcs_read((u32)rcu_trig_recv_virtbase, u32Count, (u32*)buffer);
 
-	*buffer = readl(rcu_trig_recv_virtbase);
-	printk("read %lx, at 0x%p\n", *buffer, rcu_trig_recv_virtbase);
-//	iResult = initRealBuffers();
 	
 	return iResult;
 }
 
 static void __exit rcumodule_exit(void)
 {
-	cdev_del(&c_device);
-	device_destroy(c1, first);
-	class_destroy(c1);
-    unregister_chrdev_region(first, 1);
-    iounmap(rcu);
+    unregister_chrdev(rcuc_majorID, "rcuc");
+    iounmap(trig_recv_virtbase);
 	printk("Bye bye kernel\n"); // printed when succesfully closed
 }
 
