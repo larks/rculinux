caa6099841db75f9e06844411bb6592de9055d19
Lars Bratrud <uberfryx@gmail.com>
2013-12-17 15:33:59 +0100

    read procedure

--- drivers/radmonc.c
+++ drivers/radmonc.c
@@ -12,14 +12,18 @@
 //#include <linux/arch/arm/mach-m2s/gpio.h>
 #include <linux/gpio.h>
 
-/*
-#include "mrKernLogging.c"   // kernel debugging
-*/
 // Fixed major number
 static uint radmonc_majorID = 253;
 
-// Access control
-static int driver_lock = 0;
+// pin names
+
+//static struct gpio jtag_radmon[] = {
+//		{2, GPIOF_OUT_INIT_LOW, "Radmon TCK"}, /*TCK going to Radmon*/
+//		{3, GPIOF_OUT_INIT_LOW, "Radmon TDO"}, /*TDO going to Radmon TDI*/
+//		{4, GPIOF_OUT_INIT_LOW, "Radmon TMS"}, /*TMS*/
+//		{5, GPIOF_OUT_INIT_HIGH, "Radmon TRST"}, /*nTRST going to Radmon*/
+//		{6, GPIOF_IN_INIT_LOW, "Radmon TDI"} /*TDI to Radmon TDO*/
+//};
 
 /*
 * Here comes file operation functions:
@@ -27,6 +31,10 @@ static int driver_lock = 0;
 // File open
 static int radmonjtag_open(struct inode* inode, struct file* file_p)
 {
+	int i;
+	printk("hello cat!\n");
+	i = gpio_get_value(6);
+	printk("gpio_get_value=0x%X\n", i);
 	return 0;
 }
 // File close 
@@ -40,8 +48,17 @@ static ssize_t radmonjtag_read(struct file* file_p,
                          size_t count, 
                          loff_t* f_pos)
 {
-	printk("read not implemented\n");
-	return 0;
+// size_t read(int fildes, void *buf, size_t nbytes);
+// read(com_port, &data,1), ex where data=0x50
+// file_p = com_port, buffer = data, count=1	
+	
+	int ret;
+	ret = gpio_get_value(6);
+	if(copy_to_user(buffer, &ret, 1) != 0)
+		return -EFAULT;
+	else
+	//printk("ret=%d\n", ret);
+	return ret;
 }
 // File write
 static ssize_t radmonjtag_write(struct file *file_p, 
@@ -49,7 +66,11 @@ static ssize_t radmonjtag_write(struct file *file_p,
                           size_t length, 
                           loff_t* f_pos)
 {
-	printk("write not implemented\n");
+//	ssize_t write(int fildes, const void *buf, size_t nbytes);
+// ex: write(com_port, &data, 1); where data=0x70;	
+    int ret; int gpio;
+    copy_from_user(&);
+//	printk("write not implemented\n");
 	return 0;	
 }
 // Define our custom file operation structure
@@ -67,7 +88,6 @@ static struct file_operations radmonc_fops =
 */
 static int __init radmonjtagmodule_init(void)
 {
-	int i;
 	int iResult = 0; // holding result of operations
 	iResult = register_chrdev(radmonc_majorID, "radmonc", &radmonc_fops);
 	if (iResult < 0) {
@@ -75,31 +95,25 @@ static int __init radmonjtagmodule_init(void)
 		//mrlogmessage(LOG_ERROR, KERN_ERR "module init: can't register driver");
 	}
 	else{
-		printk(KERN_INFO "radmonc: module registerd\n");
-		for (i=0; i<5; i++){
-			iResult = gpio_request(i, "GPIO Jtag");
-			if(iResult<0){
-                printk(KERN_WARNING "radmonc: unable to request GPIO_%d\n", i);
-            }
-			else{
-                printk(KERN_INFO "requested GPIO: i=%d, iResult=%d\n", i, iResult);
-            }
-		}
+		iResult = gpio_request(6, "TDI");
+		printk(KERN_INFO "radmonc: module registered, iResult=%d\n", iResult);
+		iResult = gpio_direction_input(6);
+		printk("radmonc: gpio_direction, iResult=%d\n", iResult);
 	}			
 	return iResult;
 }
 
 static void __exit radmonjtagmodule_exit(void)
 {
-	int i;
+	int iResult;
 	//int iResult;
     unregister_chrdev(radmonc_majorID, "rcuc");
-    for (i=0; i<5; i++){
-		gpio_free(i);
+//    for (i=0; i<5; i++){
+		gpio_free(6);
 		//if(iResult<0){printk(KERN_WARNING "radmonc: unable to request GPIO_%d\n", i);}
 		//else {printk(KERN_INFO "freed GPIO: i=%d, iResult=%d\n", i, iResult);}
-        printk("Freed GPIO_%d\n",i);
-	}
+        printk("Freed GPIO, iResult=%d\n",iResult);
+//	}
 
 	printk("Bye bye kernel\n"); // printed when succesfully closed
 }
