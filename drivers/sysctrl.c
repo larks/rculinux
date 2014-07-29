ee87bc527022ed0465edebda59d78ab77d626402
Lars Bratrud <uberfryx@gmail.com>
2014-07-29 15:25:21 +0200

    sysctrl module ramfs entry

--- drivers/sysctrl.c
+++ drivers/sysctrl.c
@@ -1,8 +1,21 @@
-/*
- * sysctrl.c - The simplest loadable kernel module.
- * Intended as a template for development of more
- * meaningful kernel modules.
- */
+/************************************************************************
+**
+**
+** This file is property of and copyright by the Experimental Nuclear 
+** Physics Group, Dep. of Physics and Technology
+** University of Bergen, Norway, 2014
+** This file has been written by Lars Bratrud,
+** Lars.Bratrud@cern.ch
+**
+** Permission to use, copy, modify and distribute this software and its  
+** documentation strictly for non-commercial purposes is hereby granted  
+** without fee, provided that the above copyright notice appears in all  
+** copies and that both the copyright notice and this permission notice  
+** appear in the supporting documentation. The authors make no claims    
+** about the suitability of this software for any purpose. It is         
+** provided "as is" without express or implied warranty.                 
+**
+*************************************************************************/
 
 #include <linux/module.h>
 #include <linux/kernel.h>
